23d8ce03e63b4882ec9341e9d9e2e92f8374b428
matthew <18148886@VCCTSTDST4PC38>
2018-02-12 15:52:07 +0200

    Added a comment :)

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstString;
+            string secondString;
+            double firstNumber;
+            double secondNumber;
+            double total;
+
+            Console.WriteLine("Calculator");
+
+            Console.Write("First number: ");
+
+            firstString = Console.ReadLine();
+
+            firstNumber = Convert.ToDouble(firstString);
+
+           Console.WriteLine();
+            //hello
+
+             Console.Write("Second Number: ");
+            secondString = Console.ReadLine();
+            secondNumber = Convert.ToDouble(secondString);
+           total = firstNumber + secondNumber;
+           Console.WriteLine(firstNumber + "+" + secondNumber + "=" + total);
+            Console.ReadKey();
+        }
+    }
+}
